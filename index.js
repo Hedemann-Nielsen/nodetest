87088360668eeb9f2ce9d20d82fd069b81e6b1a5
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2023-11-26 17:40:07 +0100

    1. commit

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-import express from "express";
+import express, { query } from "express";
 
 const app = express();
-app.get("/", (request, response) => {
-	console.log(request.query);
-	response.send("Forspørgsel sendt");
+
+app.get("/", (req, res) => {
+	console.log(req, query);
+	res.send("Forspørgsel sendt");
 });
+
 app.get("/about", (req, res) => {
-	res.send("Om os");
+	console.log(req.query);
+	res.send("om os");
 });
-app.get("/products/:id([0-9]*)", (req, res) => {
-	console.log(req.params.id);
-	res.send("Prdukter");
-});
-// kan fanges i browser med http://localhost:4000/products/430 430=id kan være et hvilket som helst tal. ([0-9]*) er en regex
 app.listen(4000, () => {
-	console.log("server med express siger hej");
+	console.log("Server started on port 4000");
 });
